47bb2db60bf45ffff19cb084b545af3d9529829c
romanbond <romanjbond@gmail.com>
2016-06-27 16:29:37 -0400

    Working Test Code

--- AlphaEncyption.java
+++ AlphaEncyption.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//@author roman.bond
+
 package javadistanceapplication;
 
-/**
- *
- * @author roman.bond
- */
 public class AlphaEncyption {
-    
+    public static void main (String[] args){
+        AlphaEncyption obj = new AlphaEncyption();
+        Scanner in = new Scanner(System.in);
+        String s;
+        s = in.nextLine();
+
+        //convertAlpha(n);
+        System.out.println("Just a test -- " + convertAlpha(s));
+    }
+
+    private static final String[] alphaNames = {
+        "",
+        " Alpha ",
+        " Bravo ",
+        " Charlie ",
+        " Delta ",
+        " Echo ",
+        " Foxtrot ",
+        " Golf ",
+        " Hotel ",
+        " India ",
+        " Juliet ",
+        " Kilo ",
+        " Lima ",
+        " Mike ",
+        " November ",
+        " Oscar ",
+        " Papa ",
+        " Quebec ",
+        " Romeo ",
+        " Sierra ",
+        " Tango ",
+        " Uniform ",
+        " Victor ",
+        " X-ray ",
+        " Yankee ",
+        " Zulu "
+    };
+
+    /*
+    private String convertLetter(String letter) {
+
+    };*/
+
+    public static String convertAlpha(String alpha) {
+        String current;
+        String Str1 = new String(alpha);
+        Str1.length();
+        if(Str1.length()>5){
+            current = "UP";
+        }else{
+            current = "DOWN";
+        }
+        /*if (number % 100 < 20){
+            current = alphaNames[number % 100];
+            number /= 100;
+        }else{
+            current = alphaNames[number % 10];
+            number /= 10;
+            current = alphaNames[number % 10] + current;
+            number /= 10;
+        }
+        if (number == 0) return current;*/
+        //return alphaNames[number] + "hundred" + current;
+                return current;
+        };
 }
